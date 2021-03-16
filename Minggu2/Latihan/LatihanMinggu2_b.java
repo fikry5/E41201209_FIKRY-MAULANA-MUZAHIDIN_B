7ce138dc4a1a588a80e52e3c8ecba053216e35a8
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-03-16 20:03:44 +0700

    Create LatihanMinggu2_b.java

---
+++ Minggu2/Latihan/LatihanMinggu2_b.java
@@ -0,0 +1,22 @@
+
+package LatihanMinggu2;
+
+/**
+ * @fikry
+ */
+
+public class LatihanMinggu2_b {
+    public static void main(String[] args) {
+        
+       String matkul = "Matematika";
+       int kode = 123;
+       char abjad = 'a';
+       double nilai = 8.5;
+       
+        System.out.println(matkul);
+        System.out.println(kode);
+        System.out.println(abjad);
+        System.out.println(nilai);
+    }
+    
+}
