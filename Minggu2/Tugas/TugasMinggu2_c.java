3d50c6c9c24e71f9784e12ee73673ed342fa650e
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-03-16 20:04:40 +0700

    Create TugasMinggu2_c.java

---
+++ Minggu2/Tugas/TugasMinggu2_c.java
@@ -0,0 +1,29 @@
+
+package TugasMinggu2;
+
+/**
+ * @fikry
+ */
+
+public class TugasMinggu2_c {
+    public static void main(String[] args) {
+        
+        boolean gameOver = false;
+        int students=50,classes=3;
+        double sales_tax;
+        short number1;
+        
+        int 2beOrNot2be; 
+        // Nama variable tidak boleh diawali oleh angka (harus diawali oleh huruf)
+        float price index; 
+        /*Nama variable tidak boleh menggunakan spasi, jika nama  variable lebih
+        dari satu kata maka dapat menggunakan tanda ( _ ) sebagai penghubung antar kata */
+        double lastYear'sPrice; 
+        /*Nama variable tidak menggunakan tanda petik. Jika terdapat tanda petik pembuka, 
+        maka harus ada tanda petik penutup. Tanda petik satu biasanya digunakan untuk mengisi variable char */
+        long class;
+        /*Kata class merupakan salah satu keyword yang terdapat pada java, sehingga tidak boleh digunakan
+        untuk nama variable. Kita dapat menggunakan kata class namun harus menambahkan nomor dibelakangnya seperti class2*/
+    }
+}
+
