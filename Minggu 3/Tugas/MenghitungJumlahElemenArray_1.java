0b82b1d283ff36187bb371ba4b25017b7791763f
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-03-23 16:05:16 +0700

    Create MenghitungJumlahElemenArray_1.java

---
+++ Minggu 3/Tugas/MenghitungJumlahElemenArray_1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LatihanMinggu3;
+
+/**
+ *
+ * @author hp
+ */
+public class MenghitungJumlahElemenArray_1 {
+     public static void main(String[] args) {
+        int[] nilai = {3, 7, 8, 1, 10, 20, 35};
+        
+        System.out.println("Jumlah elemen array nilai =  " + nilai.length);
+     }
+}
+     
