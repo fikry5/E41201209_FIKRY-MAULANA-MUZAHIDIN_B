131d241f910417f1a80a7cc1cf44707b3a138d74
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-03-30 10:34:38 +0700

    Create LatihanWhile.java

---
+++ Minggu 4/Latihan/LatihanWhile.java
@@ -0,0 +1,18 @@
+
+package LatihanMinggu4;
+
+/**
+ * @fikry
+ */
+public class LatihanWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 10) {
+            // blok kode yang akan diulang
+            System.out.println("Perulangan ke-" + i);
+            // increment nilai i
+            i++;
+        }
+    }
+}
