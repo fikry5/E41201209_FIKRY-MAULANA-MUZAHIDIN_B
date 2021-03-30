8aaca4c7e1adb7f4354afca086041baa62f2fb30
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-03-30 10:34:17 +0700

    Create LatihanFor.java

---
+++ Minggu 4/Latihan/LatihanFor.java
@@ -0,0 +1,17 @@
+
+package LatihanMinggu4;
+
+/**
+ * @fikry
+ */
+public class LatihanFor {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 5; i++) {
+                for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
