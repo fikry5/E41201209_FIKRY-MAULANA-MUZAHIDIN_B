79e0d9af0a7c17c48389c490a8bccb5e04a55f77
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-03-30 10:34:07 +0700

    Create LatihanDoWhlie.java

---
+++ Minggu 4/Latihan/LatihanDoWhlie.java
@@ -0,0 +1,18 @@
+
+package LatihanMinggu4;
+
+/**
+ * @fikry
+ */
+public class LatihanDoWhlie {
+    
+    public static void main(String[] args) {
+        // membuat variabel
+        int i = 0;
+        
+        do {
+            System.out.println("Perulangan ke- " + i);
+            i++;
+        } while ( i <= 10);
+    }
+}
