d71a9b240e35762e8fe955724f4c974e7cf39997
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-03-30 10:34:28 +0700

    Create LatihanForEach.java

---
+++ Minggu 4/Latihan/LatihanForEach.java
@@ -0,0 +1,18 @@
+
+package LatihanMinggu4;
+
+/**
+ * @fikry
+ */
+public class LatihanForEach {
+    public static void main(String[] args) {
+        
+        // membuat array
+        int angka[] = {3,1,42,24,12};
+        
+        // menggunakan perulangan For each untuk menampilkan angka
+        for ( int x : angka ) {
+            System.out.print(x + " ");
+        }
+    }
+}
