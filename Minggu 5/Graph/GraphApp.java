cf2a0279e047c4a211543353547de0003fb3adb8
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-10 18:18:39 +0700

    Create GraphApp.java

---
+++ Minggu 5/Graph/GraphApp.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasMinggu5.graph;
+
+/**
+ *
+ * @author hp
+ */
+public class GraphApp {
+    public static void main(String[] args) {
+        
+        Graph theGraph = new Graph();
+        
+        theGraph.addVertex('A');//0
+        theGraph.addVertex('B');//1
+        theGraph.addVertex('C');//2
+        theGraph.addVertex('D');//3
+        theGraph.addVertex('E');//4        
+        
+        //BFS
+        theGraph.addEdge(0, 1);//AB
+        theGraph.addEdge(1, 2);//BC
+        theGraph.addEdge(2, 3);//cD
+        theGraph.addEdge(3, 4);//DE
+        theGraph.bfs();
+        System.out.println("");
+        theGraph.display();
+        
+        //DFS
+        theGraph.addEdge(0, 1);//AB
+        theGraph.addEdge(1, 2);//BC
+        theGraph.addEdge(0, 3);//AD
+        theGraph.addEdge(3, 4);//DE
+        theGraph.dfs();
+        System.out.println("");
+        theGraph.display();
+    }
+}
