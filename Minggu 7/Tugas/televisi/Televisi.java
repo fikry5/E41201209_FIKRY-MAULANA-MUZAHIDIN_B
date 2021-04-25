c6982cf1da90278b38a83677d04b293c6dff2189
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:37:06 +0700

    Create Televisi.java

---
+++ Minggu 7/Tugas/televisi/Televisi.java
@@ -0,0 +1,77 @@
+
+package TugasMinggu7.televisi;
+
+/**
+ *
+ * @author Fikry
+ */
+public class Televisi {
+    public String deskripsi;
+    public int jumlahChannel;
+    public String[] channel;
+    public int ChannelAktif;
+    public int volume;
+    public String TELETEXT;
+    public String modusTampilan;
+    public String TV;
+
+    //setting deskripsi dan jumlah channel
+    public Televisi ( String deskripsi, int jumlahChannel) { 
+                this.deskripsi = deskripsi;
+                this.jumlahChannel = jumlahChannel;   
+    }
+   
+    public String getDeskripsi(){   
+       return deskripsi;
+    }
+   
+    public int getJumlahChannels() {       
+        return jumlahChannel;
+    }
+    
+    public String[] getChannels(){
+        if (channel == null){
+            System.out.println("Belum ada channel yang di set!");
+        }     
+        this.channel = channel;      
+        return channel;
+    }
+    
+    //setting channel yang ada
+    public void setChannels(String[] channel){
+        
+        if (channel.length > jumlahChannel){
+            System.out.println("Maaf TV ini hanya dapat menyimpan : " 
+                + jumlahChannel);
+        }     
+        else {
+            this.channel = channel;
+            System.out.println("Informasi channel berhasil diupdate!");
+        }
+    }
+    
+    //setting channel aktif
+    public int getChannelAktif(){
+        return ChannelAktif;
+    }
+    
+    public void setChannelAktif(int ChannelAktif){
+    
+        if ( ChannelAktif > channel.length){
+            System.out.println("Channel yang anda inginkan belum diset!");
+            return;
+        }
+        
+        this.ChannelAktif = ChannelAktif;
+        System.out.println("Pindah channel ke : " + channel[ChannelAktif]);
+    }
+    
+    public int getVolume(){
+        return volume;
+    }
+    
+    public void setVolume(int volume){
+        this.volume = volume;
+        System.out.println("Intensitas volume sekarang : " + volume);
+    }   
+}
