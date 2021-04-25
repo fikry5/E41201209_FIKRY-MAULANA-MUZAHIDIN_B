dc6e1f401b05d674d0f13d9b45982eec8027658a
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:34:21 +0700

    Create Test.java

---
+++ Minggu 7/Latihan/Orang/Test.java
@@ -0,0 +1,13 @@
+
+package Orang;
+
+/**
+ *
+ * @author fikry
+ */
+public class Test {
+     public static void main(String[] args){
+        Pelajar mahasiswa = new Pelajar("Fikry", 165 , 53, "E41201209", "Politeknik Negeri Jember",98.5);
+        System.out.println(mahasiswa.toString());
+    }
+}
