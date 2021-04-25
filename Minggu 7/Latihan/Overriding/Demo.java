61ea6d8c257c8dc062193395f71e892e3f6d4abb
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:35:16 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan/Overriding/Demo.java
@@ -0,0 +1,13 @@
+
+package Overriding;
+
+/**
+ *
+ * @author fikry
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B subOb = new B();
+        subOb.tampilkanKeLayar();               
+    }
+}
