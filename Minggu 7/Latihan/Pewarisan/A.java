09fbc23c161e12f45a9da4a02acbf609f2f85b3d
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:35:25 +0700

    Create A.java

---
+++ Minggu 7/Latihan/Pewarisan/A.java
@@ -0,0 +1,17 @@
+
+package Pewarisan;
+
+/**
+ *
+ * @author fikry
+ */
+public class A {
+    
+    int x;
+    int y;
+
+    void TampilkanNilaiXY() {
+        System.out.println("Nilai X : " + x + " , y : " + y);
+    }
+    
+}
