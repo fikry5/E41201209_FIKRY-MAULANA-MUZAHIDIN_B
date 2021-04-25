b55497b7e89a6f2befc217867c66d10b9f573d90
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:34:37 +0700

    Create B.java

---
+++ Minggu 7/Latihan/Overriding/B.java
@@ -0,0 +1,12 @@
+
+package Overriding;
+
+/**
+ *
+ * @author fikry
+ */
+public class B extends A {
+        public void tampilkanKeLayar(){
+            System.out.println("Method milik class B dipanggil");
+        }
+}
