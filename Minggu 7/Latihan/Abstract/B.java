2bed4bba92699a153765f8c752a528b9f8a44215
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:31:31 +0700

    Create B.java

---
+++ Minggu 7/Latihan/Abstract/B.java
@@ -0,0 +1,12 @@
+
+package Abstact;
+
+/**
+ *
+ * @author fikry
+ */
+public class B extends A {
+    public void method2(){
+        System.out.println("Method abstract yang sudah menjadi konkrit dalam class B");
+    }
+}
