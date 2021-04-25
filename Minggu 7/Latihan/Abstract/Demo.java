2bc0f22c4ca573edf9685249b26b080f3e5101df
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:32:20 +0700

    Create Demo.java

---
+++ Minggu 7/Latihan/Abstract/Demo.java
@@ -0,0 +1,14 @@
+
+package Abstact;
+
+/**
+ *
+ * @author fikry
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.method1();
+        ob.method2();
+    }
+}
