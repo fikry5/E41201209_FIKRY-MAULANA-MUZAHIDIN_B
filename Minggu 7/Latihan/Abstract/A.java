998be5f87842794ff883eb6115b58da0ac8a5819
Fikry M <75301621+fikry5@users.noreply.github.com>
2021-04-25 08:29:56 +0700

    Create A.java

---
+++ Minggu 7/Latihan/Abstract/A.java
@@ -0,0 +1,16 @@
+
+package Abstact;
+
+/**
+ *
+ * @author fikry
+ */
+public abstract class A {
+    abstract public void method2();// deklarasi method abstract
+    //metode konkrit yang memiliki implementasi
+
+    public void method1() {
+        System.out.println("Method konkrit dari class A");
+    }
+
+}
